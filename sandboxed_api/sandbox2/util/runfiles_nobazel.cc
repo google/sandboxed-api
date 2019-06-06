24c3e34344924091bfcf8adf9fb9f480c0a10321
Christian Blichmann <cblichmann@google.com>
2019-06-06 05:44:10 -0700

    Implement a GetDataDependencyFilepath() for CMake (non-Bazel really).
    This code assumes, like Bazel's runfiles that the data dependency to access
    exists in the same sub-tree as the binary:
    
    WORKSPACE
    +- sandboxed_api/sandbox2/examples/crc4
                                       +- crc4bin
                                       +- crc4sandbox
    
    The code requires the directory structure to exist, so that in the example
    above, crc4sandbox can use
      GetDataDependencyFilepath("sandboxed_api/sandbox2/examples/crc4/crc4bin")
    
    regardless of how it was called.
    
    PiperOrigin-RevId: 251834480
    Change-Id: I6470b62ce9b403297116481a0c17c070992f2e81

---
+++ sandboxed_api/sandbox2/util/runfiles_nobazel.cc
@@ -0,0 +1,47 @@
+// Copyright 2019 Google LLC. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <unistd.h>
+
+#include <cstdlib>
+
+#include "absl/strings/str_format.h"
+#include "absl/strings/strip.h"
+#include "sandboxed_api/sandbox2/util/path.h"
+#include "sandboxed_api/sandbox2/util/runfiles.h"
+#include "sandboxed_api/util/raw_logging.h"
+
+namespace sandbox2 {
+
+std::string GetDataDependencyFilePath(absl::string_view relative_path) {
+  static std::string* base_dir = []() {
+    std::string link_name(PATH_MAX, '\0');
+    SAPI_RAW_PCHECK(
+        readlink("/proc/self/exe", &link_name[0], link_name.size()) != -1, "");
+    link_name.resize(link_name.find_first_of('\0'));
+    return new std::string(file::SplitPath(link_name).first);
+  }();
+  absl::string_view resolved =
+      absl::StripSuffix(*base_dir, file::SplitPath(relative_path).first);
+  return file::JoinPath(resolved, relative_path);
+}
+
+std::string GetInternalDataDependencyFilePath(absl::string_view relative_path) {
+  // The Bazel version has an additional "com_google_sandboxed_api" path
+  // component.
+  return GetDataDependencyFilePath(
+      file::JoinPath("sandboxed_api", relative_path));
+}
+
+}  // namespace sandbox2
