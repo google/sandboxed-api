google/sandboxed-api

commit 6edcf5f8238aea42e66e6b8a60871d801699975a

Internal change.
PiperOrigin-RevId: 256378370
Change-Id: I1b00662a4c4c743ab79ecf35e857e4f7bc9f5e1c

sandboxed_api/sandbox2/logserver.cc
@@ -34,9 +34,9 @@ void LogServer::Run() {
       severity = logging::ERROR;
       fatal_string = " FATAL";
     }
-    logging::LogMessage log_message(msg.path().c_str(), msg.line(), severity);
-    log_message.stream() << "(sandboxee " << msg.pid() << fatal_string
-                         << "): " << msg.message();
+    logging::LogMessage(msg.path().c_str(), msg.line(), severity);
+    log_message.stream()
+        << "(sandboxee " << msg.pid() << fatal_string << "): " << msg.message();
   }
 
   LOG(INFO) << "Receive failed, shutting down LogServer";

commit 2301e05097818734f59b881d7fbe1624c17fc840

Follow-up to rev. 6edcf5f which introduced a build failure
PiperOrigin-RevId: 256961234
Change-Id: I312d054c5d35413b2e6b061ddfa30c80397b19eb

sandboxed_api/sandbox2/logserver.cc
@@ -34,7 +34,7 @@ void LogServer::Run() {
       severity = logging::ERROR;
       fatal_string = " FATAL";
     }
-    logging::LogMessage(msg.path().c_str(), msg.line(), severity);
+    logging::LogMessage log_message(msg.path().c_str(), msg.line(), severity);
     log_message.stream()
         << "(sandboxee " << msg.pid() << fatal_string << "): " << msg.message();
   }