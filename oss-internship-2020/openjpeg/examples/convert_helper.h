5598690320af1dec58a3c7ad30fe3a6ff4fb9bd8
Katarzyna Miernikiewicz <kasia.mie@interia.pl>
2020-08-14 10:38:27 +0000

    added comments; added Google formatting style

--- oss-internship-2020/openjpeg/examples/convert_helper.h
+++ oss-internship-2020/openjpeg/examples/convert_helper.h
@@ -1,8 +1,11 @@
+// imagetopnm and the two functions it calls internaly are copied from the
+// library's tools; from openjpeg/src/bin/jp2/convert.c
+
 #include "openjp2_sapi.sapi.h"
 
 #define OPJ_TRUE 1
 #define OPJ_FALSE 0
 
 const char* opj_version(void);
-int imagetopnm(opj_image_t * image, const char *outfile, int force_split);
-static int are_comps_similar(opj_image_t * image);
+static int are_comps_similar(opj_image_t* image);
+int imagetopnm(opj_image_t* image, const char* outfile, int force_split);
--- oss-internship-2020/openjpeg/examples/decompress_example.cc
+++ oss-internship-2020/openjpeg/examples/decompress_example.cc
@@ -12,19 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
-// Perform decompression from *.jp2 to *.pnm format 
-
+// Perform decompression from *.jp2 to *.pnm format
 
 #include <libgen.h>
 #include <syscall.h>
 
-#include <iostream>
-#include <cstdlib>
 #include <cassert>
+#include <cstdlib>
+#include <iostream>
 
 #include "convert_helper.h"
-
 #include "openjp2_sapi.sapi.h"
 #include "sandboxed_api/util/flag.h"
 
@@ -33,8 +30,7 @@ class Opj_image_t : public sapi::v::Struct<opj_image_t> {};
 
 class Openjp2SapiSandbox : public Openjp2Sandbox {
  public:
-  Openjp2SapiSandbox(const std::string& in_file)
-      : in_file_(in_file) {}
+  Openjp2SapiSandbox(const std::string& in_file) : in_file_(in_file) {}
 
   std::unique_ptr<sandbox2::Policy> ModifyPolicy(
       sandbox2::PolicyBuilder*) override {
@@ -47,9 +43,9 @@ class Openjp2SapiSandbox : public Openjp2Sandbox {
         .AllowSystemMalloc()
         .AllowExit()
         .AllowSyscalls({
-          __NR_futex,
-          __NR_close,
-          __NR_lseek,
+            __NR_futex,
+            __NR_close,
+            __NR_lseek,
         })
         .AddFile(in_file_)
         .BuildOrDie();
@@ -59,105 +55,105 @@ class Openjp2SapiSandbox : public Openjp2Sandbox {
   std::string in_file_;
 };
 
-
 int main(int argc, char* argv[]) {
-	gflags::ParseCommandLineFlags(&argc, &argv, true);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
 
-	if (argc != 3) {
-		std::cerr << "usage: " 
-				  << basename(argv[0]) 
-				  << " absolute/path/to/INPUT.jp2"
-				  << " absolute/path/to/OUTPUT.pnm\n";
-		return EXIT_FAILURE;
-	}
+  if (argc != 3) {
+    std::cerr << "usage: " << basename(argv[0]) << " absolute/path/to/INPUT.jp2"
+              << " absolute/path/to/OUTPUT.pnm\n";
+    return EXIT_FAILURE;
+  }
 
-	std::string in_file(argv[1]);
+  std::string in_file(argv[1]);
 
-	Openjp2SapiSandbox sandbox(in_file);
-	absl::Status status = sandbox.Init();
-	assert(status.ok());
+  // initialize sandbox
+  Openjp2SapiSandbox sandbox(in_file);
+  absl::Status status = sandbox.Init();
+  assert(status.ok());
 
-	Openjp2Api api(&sandbox);
-	sapi::v::ConstCStr in_file_v(in_file.c_str());
+  Openjp2Api api(&sandbox);
+  sapi::v::ConstCStr in_file_v(in_file.c_str());
 
-	sapi::StatusOr<opj_stream_t*> stream_status 
-		= api.opj_stream_create_default_file_stream(in_file_v.PtrBefore(), 1);
-	assert(stream_status.ok());
-	void* stream_status_value = stream_status.value();
-	sapi::v::RemotePtr stream_pointer(stream_status_value);
+  // initialize library's main data-holders
+  sapi::StatusOr<opj_stream_t*> stream_status =
+      api.opj_stream_create_default_file_stream(in_file_v.PtrBefore(), 1);
+  assert(stream_status.ok());
+  void* stream_status_value = stream_status.value();
+  sapi::v::RemotePtr stream_pointer(stream_status_value);
 
-	sapi::StatusOr<opj_codec_t*> codec_status 
-		= api.opj_create_decompress(OPJ_CODEC_JP2);
-	assert(codec_status.ok());
-	void* codec_status_value = codec_status.value();
-	sapi::v::RemotePtr codec_pointer(codec_status_value);
+  sapi::StatusOr<opj_codec_t*> codec_status =
+      api.opj_create_decompress(OPJ_CODEC_JP2);
+  assert(codec_status.ok());
+  void* codec_status_value = codec_status.value();
+  sapi::v::RemotePtr codec_pointer(codec_status_value);
 
-	Parameters parameters;
-	status = api.opj_set_default_decoder_parameters(parameters.PtrBoth());
-	assert(status.ok());
+  Parameters parameters;
+  status = api.opj_set_default_decoder_parameters(parameters.PtrBoth());
+  assert(status.ok());
 
-	sapi::StatusOr<OPJ_BOOL> bool_status 
-		= api.opj_setup_decoder(&codec_pointer, parameters.PtrBefore());
-	assert(bool_status.ok());
-	assert(bool_status.value());
+  sapi::StatusOr<OPJ_BOOL> bool_status =
+      api.opj_setup_decoder(&codec_pointer, parameters.PtrBefore());
+  assert(bool_status.ok());
+  assert(bool_status.value());
 
-	sapi::v::GenericPtr image_pointer;
-	bool_status = api.opj_read_header(&stream_pointer,
-									  &codec_pointer,
-									  image_pointer.PtrAfter());
-	assert(bool_status.ok());
-	assert(bool_status.value());
+  // start reading image from the input file
+  sapi::v::GenericPtr image_pointer;
+  bool_status = api.opj_read_header(&stream_pointer, &codec_pointer,
+                                    image_pointer.PtrAfter());
+  assert(bool_status.ok());
+  assert(bool_status.value());
 
-	Opj_image_t image;
-	image.SetRemote((void*)image_pointer.GetValue());
-	assert(sandbox.TransferFromSandboxee(&image).ok());
+  Opj_image_t image;
+  image.SetRemote((void*)image_pointer.GetValue());
+  assert(sandbox.TransferFromSandboxee(&image).ok());
 
-	bool_status = api.opj_decode(&codec_pointer,
-								 &stream_pointer,
-								 (sapi::v::Ptr*)&image_pointer);
-	assert(bool_status.ok());
-	assert(bool_status.value());
+  bool_status = api.opj_decode(&codec_pointer, &stream_pointer,
+                               (sapi::v::Ptr*)&image_pointer);
+  assert(bool_status.ok());
+  assert(bool_status.value());
 
-	bool_status = api.opj_end_decompress(&codec_pointer, &stream_pointer);
-	assert(bool_status.ok());
-	assert(bool_status.value());
+  bool_status = api.opj_end_decompress(&codec_pointer, &stream_pointer);
+  assert(bool_status.ok());
+  assert(bool_status.value());
 
-	status = api.opj_stream_destroy(&stream_pointer);
-	assert(status.ok());
+  int components = image.data().numcomps;
 
-	int components = image.data().numcomps;
+  // transfer the read data to the main process
+  sapi::v::Array<opj_image_comp_t> image_components(components);
+  image_components.SetRemote(image.data().comps);
+  assert(sandbox.TransferFromSandboxee(&image_components).ok());
 
-	sapi::v::Array<opj_image_comp_t> image_components(components);
-	image_components.SetRemote(image.data().comps);
-	assert(sandbox.TransferFromSandboxee(&image_components).ok());
+  image.mutable_data()->comps = (opj_image_comp_t*)image_components.GetLocal();
 
-	image.mutable_data()->comps
-		= (opj_image_comp_t*)image_components.GetLocal();
+  int width = (int)image.data().comps[0].w;
+  int height = (int)image.data().comps[0].h;
 
-	int width = (int)image.data().comps[0].w;
-	int height = (int)image.data().comps[0].h;
+  OPJ_INT32 data[components][width * height];
+  sapi::v::Array<OPJ_INT32> image_components_data(width * height);
 
-	int data[components][width * height];
-	sapi::v::Array<int> image_components_data(width * height);
+  for (int i = 0; i < components; i++) {
+    image_components_data.SetRemote(image.data().comps[i].data);
+    assert(sandbox.TransferFromSandboxee(&image_components_data).ok());
+    for (int j = 0; j < width * height; j++) {
+      data[i][j] = image_components_data[j];
+    }
+    image_components[i].data = data[i];
+  }
 
-	for (int i = 0; i < components; i++) {
-		image_components_data.SetRemote(image.data().comps[i].data);
-		assert(sandbox.TransferFromSandboxee(&image_components_data).ok());
-		for (int j = 0; j < width * height; j++) {
-			data[i][j] = image_components_data[j];
-		}
-		image_components[i].data = data[i];
-	}
+  // convert the image to the desired format and save it to the file
+  int error = imagetopnm((opj_image_t*)image.GetLocal(), argv[2], 0);
+  assert(error == 0);
 
-	int error = imagetopnm((opj_image_t*)image.GetLocal(), argv[2], 0);
-	assert(error == 0);
+  // cleanup
+  sapi::v::RemotePtr remote_image_pointer(image.GetRemote());
+  status = api.opj_image_destroy(&remote_image_pointer);
+  assert(status.ok());
 
-	status = api.opj_destroy_codec(&codec_pointer);
-	assert(status.ok());
+  status = api.opj_stream_destroy(&stream_pointer);
+  assert(status.ok());
 
-	sapi::v::RemotePtr remote_image_pointer(image.GetRemote());
-	status = api.opj_image_destroy(&remote_image_pointer);
-	assert(status.ok());
+  status = api.opj_destroy_codec(&codec_pointer);
+  assert(status.ok());
 
-	return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
