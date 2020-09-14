89345ada03ccfda6355b5b2d701d081305d76e3d
root <amedar@google.com>
2020-09-14 14:39:16 +0000

    added a part of the example code

--- oss-internship-2020/sapi_libarchive/examples/sapi_example.cc
+++ oss-internship-2020/sapi_libarchive/examples/sapi_example.cc
@@ -1,8 +1,18 @@
 #include <iostream>
+
 #include "libarchive_sapi.sapi.h"
 
-
 int main() {
-    std::cout << "WORKS2" << std::endl;
-    return 0;
+  std::cout << "WORKS2" << std::endl;
+
+    LibarchiveSandbox sandbox;
+    sandbox.Init().IgnoreError();
+    LibarchiveApi api(&sandbox);
+
+    if (api.archive_write_disk_new().ok()) {
+        std::cout << "OK" << std::endl;
+    }
+
+
+  return 0;
 }
