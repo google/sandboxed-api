519b5af157876828d92764f27bde592afa99f537
Andrei Medar <amedar@google.com>
2020-10-08 10:58:21 +0000

    Added ld_preload example usage

---
+++ oss-internship-2020/libarchive/ld_preload_example/minitar_main.cc
@@ -0,0 +1,195 @@
+/*-
+ * This file is in the public domain.
+ * Do with it as you will.
+ */
+
+/*-
+ * This is a compact "tar" program whose primary goal is small size.
+ * Statically linked, it can be very small indeed.  This serves a number
+ * of goals:
+ *   o a testbed for libarchive (to check for link pollution),
+ *   o a useful tool for space-constrained systems (boot floppies, etc),
+ *   o a place to experiment with new implementation ideas for bsdtar,
+ *   o a small program to demonstrate libarchive usage.
+ *
+ * Use the following macros to suppress features:
+ *   NO_BZIP2 - Implies NO_BZIP2_CREATE and NO_BZIP2_EXTRACT
+ *   NO_BZIP2_CREATE - Suppress bzip2 compression support.
+ *   NO_BZIP2_EXTRACT - Suppress bzip2 auto-detection and decompression.
+ *   NO_COMPRESS - Implies NO_COMPRESS_CREATE and NO_COMPRESS_EXTRACT
+ *   NO_COMPRESS_CREATE - Suppress compress(1) compression support
+ *   NO_COMPRESS_EXTRACT - Suppress compress(1) auto-detect and decompression.
+ *   NO_CREATE - Suppress all archive creation support.
+ *   NO_CPIO_EXTRACT - Suppress auto-detect and dearchiving of cpio archives.
+ *   NO_GZIP - Implies NO_GZIP_CREATE and NO_GZIP_EXTRACT
+ *   NO_GZIP_CREATE - Suppress gzip compression support.
+ *   NO_GZIP_EXTRACT - Suppress gzip auto-detection and decompression.
+ *   NO_LOOKUP - Try to avoid getpw/getgr routines, which can be very large
+ *   NO_TAR_EXTRACT - Suppress tar extraction
+ *
+ * With all of the above macros defined (except NO_TAR_EXTRACT), you
+ * get a very small program that can recognize and extract essentially
+ * any uncompressed tar archive.  On FreeBSD 5.1, this minimal program
+ * is under 64k, statically linked, which compares rather favorably to
+ *         main(){printf("hello, world");}
+ * which is over 60k statically linked on the same operating system.
+ * Without any of the above macros, you get a static executable of
+ * about 180k with a lot of very sophisticated modern features.
+ * Obviously, it's trivial to add support for ISO, Zip, mtree,
+ * lzma/xz, etc.  Just fill in the appropriate setup calls.
+ */
+
+#include "minitar.h"  // NOLINT(build/include)
+
+/*
+ * NO_CREATE implies NO_BZIP2_CREATE and NO_GZIP_CREATE and NO_COMPRESS_CREATE.
+ */
+#ifdef NO_CREATE
+#undef NO_BZIP2_CREATE
+#define NO_BZIP2_CREATE
+#undef NO_COMPRESS_CREATE
+#define NO_COMPRESS_CREATE
+#undef NO_GZIP_CREATE
+#define NO_GZIP_CREATE
+#endif
+
+/*
+ * The combination of NO_BZIP2_CREATE and NO_BZIP2_EXTRACT is
+ * equivalent to NO_BZIP2.
+ */
+#ifdef NO_BZIP2_CREATE
+#ifdef NO_BZIP2_EXTRACT
+#undef NO_BZIP2
+#define NO_BZIP2
+#endif
+#endif
+
+#ifdef NO_BZIP2
+#undef NO_BZIP2_EXTRACT
+#define NO_BZIP2_EXTRACT
+#undef NO_BZIP2_CREATE
+#define NO_BZIP2_CREATE
+#endif
+
+/*
+ * The combination of NO_COMPRESS_CREATE and NO_COMPRESS_EXTRACT is
+ * equivalent to NO_COMPRESS.
+ */
+#ifdef NO_COMPRESS_CREATE
+#ifdef NO_COMPRESS_EXTRACT
+#undef NO_COMPRESS
+#define NO_COMPRESS
+#endif
+#endif
+
+#ifdef NO_COMPRESS
+#undef NO_COMPRESS_EXTRACT
+#define NO_COMPRESS_EXTRACT
+#undef NO_COMPRESS_CREATE
+#define NO_COMPRESS_CREATE
+#endif
+
+/*
+ * The combination of NO_GZIP_CREATE and NO_GZIP_EXTRACT is
+ * equivalent to NO_GZIP.
+ */
+#ifdef NO_GZIP_CREATE
+#ifdef NO_GZIP_EXTRACT
+#undef NO_GZIP
+#define NO_GZIP
+#endif
+#endif
+
+#ifdef NO_GZIP
+#undef NO_GZIP_EXTRACT
+#define NO_GZIP_EXTRACT
+#undef NO_GZIP_CREATE
+#define NO_GZIP_CREATE
+#endif
+
+int main(int unused_argc, const char** argv) {
+  const char* filename = nullptr;
+  int compress;
+  int flags;
+  int mode;
+  int opt;
+
+  mode = 'x';
+  int verbose = 0;
+  compress = '\0';
+  flags = ARCHIVE_EXTRACT_TIME;
+
+  /* Among other sins, getopt(3) pulls in printf(3). */
+  while (*++argv != nullptr && **argv == '-') {
+    const char* p = *argv + 1;
+
+    while ((opt = *p++) != '\0') {
+      switch (opt) {
+#ifndef NO_CREATE
+        case 'c':
+          mode = opt;
+          break;
+#endif
+        case 'f':
+          if (*p != '\0')
+            filename = p;
+          else
+            filename = *++argv;
+          p += strlen(p);
+          break;
+#ifndef NO_BZIP2_CREATE
+        case 'j':
+          compress = opt;
+          break;
+#endif
+        case 'p':
+          flags |= ARCHIVE_EXTRACT_PERM;
+          flags |= ARCHIVE_EXTRACT_ACL;
+          flags |= ARCHIVE_EXTRACT_FFLAGS;
+          break;
+        case 't':
+          mode = opt;
+          break;
+        case 'v':
+          verbose++;
+          break;
+        case 'x':
+          mode = opt;
+          break;
+#ifndef NO_BZIP2_CREATE
+        case 'y':
+          compress = opt;
+          break;
+#endif
+#ifndef NO_COMPRESS_CREATE
+        case 'Z':
+          compress = opt;
+          break;
+#endif
+#ifndef NO_GZIP_CREATE
+        case 'z':
+          compress = opt;
+          break;
+#endif
+        default:
+          usage();
+      }
+    }
+  }
+
+  switch (mode) {
+#ifndef NO_CREATE
+    case 'c':
+      create(filename, compress, argv, verbose);
+      break;
+#endif
+    case 't':
+      extract(filename, 0, flags, verbose);
+      break;
+    case 'x':
+      extract(filename, 1, flags, verbose);
+      break;
+  }
+
+  return (0);
+}
---
+++ oss-internship-2020/libarchive/ld_preload_example/sapi_minitar.cc
@@ -0,0 +1,207 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "sapi_minitar.h"  // NOLINT(build/include)
+
+#include <dlfcn.h>
+
+#include <iostream>
+#include <memory>
+
+#include "libarchive_sapi.sapi.h"  // NOLINT(build/include)
+#include "sandbox.h"               // NOLINT(build/include)
+#include "sandboxed_api/var_array.h"
+#include "sandboxed_api/var_ptr.h"
+
+std::unique_ptr<SapiLibarchiveSandboxExtract> sandbox_extract;
+std::unique_ptr<LibarchiveApi> api;
+
+struct ExtractTempDirectoryCleanup {
+  ExtractTempDirectoryCleanup(const std::string& dir) : dir_(dir) {}
+  ~ExtractTempDirectoryCleanup() {
+    sandbox2::file_util::fileops::DeleteRecursively(dir_);
+  }
+
+ private:
+  std::string dir_;
+};
+
+std::unique_ptr<ExtractTempDirectoryCleanup> cleanup_ptr;
+
+typedef void (*real_extract)(const char*, int, int, int);
+
+void extract(const char* filename, int do_extract, int flags, int verbose) {
+  // Here we initialize the sandbox and other objects.
+  std::string tmp_dir;
+  if (do_extract) {
+    tmp_dir = CreateTempDirAtCWD().value();
+    cleanup_ptr = absl::make_unique<ExtractTempDirectoryCleanup>(tmp_dir);
+  }
+
+  std::string filename_absolute = MakeAbsolutePathAtCWD(filename);
+
+  // Initialize sandbox and api objects.
+  sandbox_extract = absl::make_unique<SapiLibarchiveSandboxExtract>(
+      filename_absolute, do_extract, tmp_dir);
+  CHECK(sandbox_extract->Init().ok()) << "Error during sandbox initialization";
+  api = absl::make_unique<LibarchiveApi>(sandbox_extract.get());
+
+  // After everything is set up, call the original function (next symbol).
+
+  // TODO getting the mangled name of the function at runtime does not work
+  // as intended. At the moment just use the symbol directly.
+  const char* y = "_Z7extractPKciii";
+  void* x = dlsym(RTLD_NEXT, y);
+
+  CHECK(x != nullptr) << "dlsym call could not find function symbol";
+  ((real_extract)x)(filename_absolute.c_str(), do_extract, flags, verbose);
+}
+
+archive* archive_read_new() {
+  archive* ret = api->archive_read_new().value();
+  CHECK(ret != nullptr) << "Failed to create archive";
+  return ret;
+}
+
+archive* archive_write_disk_new() {
+  archive* ret = api->archive_write_disk_new().value();
+  CHECK(ret != nullptr) << "Failed to create archive";
+  return ret;
+}
+
+int archive_write_disk_set_options(archive* ext, int flags) {
+  sapi::v::RemotePtr ext_ptr(ext);
+
+  return api->archive_write_disk_set_options(&ext_ptr, flags).value();
+}
+
+int archive_read_support_filter_bzip2(archive* a) {
+  sapi::v::RemotePtr a_ptr(a);
+  return api->archive_read_support_filter_bzip2(&a_ptr).value();
+}
+
+int archive_read_support_filter_gzip(archive* a) {
+  sapi::v::RemotePtr a_ptr(a);
+  return api->archive_read_support_filter_gzip(&a_ptr).value();
+}
+
+int archive_read_support_filter_compress(archive* a) {
+  sapi::v::RemotePtr a_ptr(a);
+  return api->archive_read_support_filter_compress(&a_ptr).value();
+}
+int archive_read_support_format_tar(archive* a) {
+  sapi::v::RemotePtr a_ptr(a);
+  return api->archive_read_support_format_tar(&a_ptr).value();
+}
+
+int archive_read_support_format_cpio(archive* a) {
+  sapi::v::RemotePtr a_ptr(a);
+  return api->archive_read_support_format_cpio(&a_ptr).value();
+}
+
+int archive_write_disk_set_standard_lookup(archive* ext) {
+  sapi::v::RemotePtr ext_ptr(ext);
+
+  return api->archive_write_disk_set_standard_lookup(&ext_ptr).value();
+}
+
+int archive_read_open_filename(archive* a, const char* _filename,
+                               size_t _block_size) {
+  sapi::v::RemotePtr a_ptr(a);
+
+  return api
+      ->archive_read_open_filename(
+          &a_ptr, sapi::v::ConstCStr(_filename).PtrBefore(), _block_size)
+      .value();
+}
+
+const char* archive_error_string(archive* a) {
+  sapi::v::RemotePtr a_ptr(a);
+  char* str = api->archive_error_string(&a_ptr).value();
+  CHECK(str != nullptr) << "Could not get error message";
+  return sandbox_extract->GetCString(sapi::v::RemotePtr(str)).value().c_str();
+}
+
+int archive_read_next_header(archive* a, archive_entry** entry) {
+  sapi::v::IntBase<archive_entry*> entry_ptr_tmp(0);
+  sapi::v::RemotePtr a_ptr(a);
+  int rc =
+      api->archive_read_next_header(&a_ptr, entry_ptr_tmp.PtrAfter()).value();
+  *entry = entry_ptr_tmp.GetValue();
+  return rc;
+}
+
+// Keep a global variable so that it does not go out of scope.
+std::string str_tmp;
+
+const char* archive_entry_pathname(archive_entry* entry) {
+  sapi::v::RemotePtr entry_ptr(entry);
+  char* str = api->archive_entry_pathname(&entry_ptr).value();
+  CHECK(str != nullptr) << "Could not get error message";
+
+  str_tmp = sandbox_extract->GetCString(sapi::v::RemotePtr(str)).value();
+
+  return str_tmp.c_str();
+}
+
+int archive_read_close(archive* a) {
+  sapi::v::RemotePtr a_ptr(a);
+  return api->archive_read_close(&a_ptr).value();
+}
+
+int archive_read_free(archive* a) {
+  sapi::v::RemotePtr a_ptr(a);
+  return api->archive_read_free(&a_ptr).value();
+}
+
+int archive_write_close(archive* a) {
+  sapi::v::RemotePtr a_ptr(a);
+  return api->archive_write_close(&a_ptr).value();
+}
+
+int archive_write_free(archive* a) {
+  sapi::v::RemotePtr a_ptr(a);
+  return api->archive_write_free(&a_ptr).value();
+}
+
+int archive_write_header(archive* a, archive_entry* entry) {
+  sapi::v::RemotePtr a_ptr(a), entry_ptr(entry);
+  return api->archive_write_header(&a_ptr, &entry_ptr).value();
+}
+
+int archive_read_data_block(archive* a, const void** buff, size_t* size,
+                            la_int64_t* offset) {
+  sapi::v::IntBase<archive_entry*> buff_ptr_tmp(0);
+  sapi::v::ULLong size_tmp;
+  sapi::v::SLLong offset_tmp;
+  sapi::v::RemotePtr a_ptr(a);
+
+  int rv =
+      api->archive_read_data_block(&a_ptr, buff_ptr_tmp.PtrAfter(),
+                                   size_tmp.PtrAfter(), offset_tmp.PtrAfter())
+          .value();
+  *buff = buff_ptr_tmp.GetValue();
+  *size = size_tmp.GetValue();
+  *offset = offset_tmp.GetValue();
+
+  return rv;
+}
+
+la_ssize_t archive_write_data_block(archive* a, const void* buff, size_t s,
+                                    la_int64_t o) {
+  sapi::v::RemotePtr buff_ptr((void*)(buff));
+  sapi::v::RemotePtr a_ptr(a);
+
+  return api->archive_write_data_block(&a_ptr, &buff_ptr, s, o).value();
+}
