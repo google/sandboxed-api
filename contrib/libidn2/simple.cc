82dbf8de5b240037a0893e20225c1b88ca9ec84c
Demi Marie Obenour <demi@invisiblethingslab.com>
2022-01-24 11:16:22 -0500

    Run clang-format
    to conform to Google’s style guide.

--- contrib/libidn2/simple.cc
+++ contrib/libidn2/simple.cc
@@ -13,24 +13,23 @@
 // limitations under the License.
 
 #include <gflags/gflags.h>
-#include <syscall.h>
-
-#include <fstream>
-#include <iostream>
-#include <cstdlib>
-
 #include <glog/logging.h>
 #include <idn2.h>
+#include <syscall.h>
+
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+
 #include "libidn2_sapi.sapi.h"  // NOLINT(build/include)
 #include "sandboxed_api/util/fileops.h"
 
 class Idn2SapiSandbox : public IDN2Sandbox {
  public:
-  Idn2SapiSandbox()
-      : IDN2Sandbox() {}
+  Idn2SapiSandbox() : IDN2Sandbox() {}
 
   std::unique_ptr<sandbox2::Policy> ModifyPolicy(
-      sandbox2::PolicyBuilder*) override {
+      sandbox2::PolicyBuilder *) override {
     return sandbox2::PolicyBuilder()
         .AllowSystemMalloc()
         .AllowRead()
@@ -52,23 +51,28 @@ class IDN2Lib {
   IDN2Api *api;
   absl::StatusOr<std::string> sapi_generic(
       const char *data,
-      absl::StatusOr<int>(IDN2Api::*cb)(sapi::v::Ptr *input, sapi::v::Ptr *output, int flags));
-  absl::StatusOr<std::string> process_errors(absl::StatusOr<int> status, sapi::v::GenericPtr &ptr);
+      absl::StatusOr<int> (IDN2Api::*cb)(sapi::v::Ptr *input,
+                                         sapi::v::Ptr *output, int flags));
+  absl::StatusOr<std::string> process_errors(absl::StatusOr<int> status,
+                                             sapi::v::GenericPtr &ptr);
+
  public:
-  IDN2Lib &operator =(IDN2Lib &other) = delete;
-  IDN2Lib &operator =(const IDN2Lib &other) = delete;
+  IDN2Lib &operator=(IDN2Lib &other) = delete;
+  IDN2Lib &operator=(const IDN2Lib &other) = delete;
   IDN2Lib(IDN2Lib &other) = delete;
   IDN2Lib(const IDN2Lib &other) = delete;
-  IDN2Lib(Idn2SapiSandbox *sandbox, IDN2Api *api) :
-    sandbox(sandbox ? sandbox : (abort(), nullptr)),
-    api(api ? api : (abort(), nullptr)) {}
-  absl::StatusOr<std::string> idn2_register_u8(const char *ulabel, const char *alabel);
+  IDN2Lib(Idn2SapiSandbox *sandbox, IDN2Api *api)
+      : sandbox(sandbox ? sandbox : (abort(), nullptr)),
+        api(api ? api : (abort(), nullptr)) {}
+  absl::StatusOr<std::string> idn2_register_u8(const char *ulabel,
+                                               const char *alabel);
   absl::StatusOr<std::string> idn2_lookup_u8(const char *data);
   absl::StatusOr<std::string> idn2_to_ascii_8z(const char *ulabel);
   absl::StatusOr<std::string> idn2_to_unicode_8z8z(const char *ulabel);
 };
 
-absl::StatusOr<std::string> IDN2Lib::process_errors(absl::StatusOr<int> untrusted_res, sapi::v::GenericPtr &ptr) {
+absl::StatusOr<std::string> IDN2Lib::process_errors(
+    absl::StatusOr<int> untrusted_res, sapi::v::GenericPtr &ptr) {
   if (!untrusted_res.ok()) {
     return untrusted_res.status();
   }
@@ -76,8 +80,7 @@ absl::StatusOr<std::string> IDN2Lib::process_errors(absl::StatusOr<int> untruste
   if (res < 0) {
     if (res == IDN2_MALLOC)
       return absl::ResourceExhaustedError("malloc() failed in libidn2");
-    if (res > -10000)
-      return absl::InvalidArgumentError(idn2_strerror(res));
+    if (res > -10000) return absl::InvalidArgumentError(idn2_strerror(res));
     return absl::InvalidArgumentError("Unexpected error");
   }
   ::sapi::v::RemotePtr p(reinterpret_cast<void *>(ptr.GetValue()));
@@ -90,30 +93,29 @@ absl::StatusOr<std::string> IDN2Lib::process_errors(absl::StatusOr<int> untruste
   return *maybe_untrusted_name;
 }
 
-absl::StatusOr<std::string> IDN2Lib::idn2_register_u8(const char *ulabel, const char *alabel) {
+absl::StatusOr<std::string> IDN2Lib::idn2_register_u8(const char *ulabel,
+                                                      const char *alabel) {
   ::std::optional<::sapi::v::ConstCStr> alabel_ptr, ulabel_ptr;
-  if (ulabel)
-    ulabel_ptr.emplace(reinterpret_cast<const char *>(ulabel));
-  if (alabel)
-    alabel_ptr.emplace(reinterpret_cast<const char *>(alabel));
+  if (ulabel) ulabel_ptr.emplace(reinterpret_cast<const char *>(ulabel));
+  if (alabel) alabel_ptr.emplace(reinterpret_cast<const char *>(alabel));
   ::sapi::v::GenericPtr ptr;
   ::sapi::v::NullPtr null_ptr;
-  const auto untrusted_res =
-    api->idn2_register_u8(ulabel ? ulabel_ptr->PtrBefore() : &null_ptr,
-                          alabel ? alabel_ptr->PtrBefore() : &null_ptr,
-                          ptr.PtrAfter(),
-                          IDN2_NFC_INPUT | IDN2_NONTRANSITIONAL);
+  const auto untrusted_res = api->idn2_register_u8(
+      ulabel ? ulabel_ptr->PtrBefore() : &null_ptr,
+      alabel ? alabel_ptr->PtrBefore() : &null_ptr, ptr.PtrAfter(),
+      IDN2_NFC_INPUT | IDN2_NONTRANSITIONAL);
   return this->process_errors(untrusted_res, ptr);
 }
 
 absl::StatusOr<std::string> IDN2Lib::sapi_generic(
     const char *data,
-    absl::StatusOr<int>(IDN2Api::*cb)(sapi::v::Ptr *input, sapi::v::Ptr *output, int flags)) {
+    absl::StatusOr<int> (IDN2Api::*cb)(sapi::v::Ptr *input,
+                                       sapi::v::Ptr *output, int flags)) {
   ::sapi::v::ConstCStr src(data);
   ::sapi::v::GenericPtr ptr;
 
-  absl::StatusOr<int> untrusted_res =
-    ((api)->*(cb))(src.PtrBefore(), ptr.PtrAfter(), IDN2_NFC_INPUT | IDN2_NONTRANSITIONAL);
+  absl::StatusOr<int> untrusted_res = ((api)->*(cb))(
+      src.PtrBefore(), ptr.PtrAfter(), IDN2_NFC_INPUT | IDN2_NONTRANSITIONAL);
   return this->process_errors(untrusted_res, ptr);
 }
 
@@ -134,6 +136,7 @@ absl::StatusOr<std::string> IDN2Lib::idn2_lookup_u8(const char *data) {
 class Idn2SapiSandboxTest : public testing::Test {
   std::optional<Idn2SapiSandbox> sandbox;
   std::optional<IDN2Api> api;
+
  protected:
   std::optional<IDN2Lib> lib;
   virtual void SetUp() override {
@@ -148,7 +151,8 @@ TEST_F(Idn2SapiSandboxTest, works_okay) {
   EXPECT_TRUE(lib->idn2_lookup_u8("β").value() == "xn--nxa");
   EXPECT_TRUE(lib->idn2_lookup_u8("ß").value() == "xn--zca");
   EXPECT_TRUE(lib->idn2_lookup_u8("straße.de").value() == "xn--strae-oqa.de");
-  EXPECT_TRUE(lib->idn2_to_unicode_8z8z("xn--strae-oqa.de").value() == "straße.de");
+  EXPECT_TRUE(lib->idn2_to_unicode_8z8z("xn--strae-oqa.de").value() ==
+              "straße.de");
 }
 TEST_F(Idn2SapiSandboxTest, register_conversion) {
   // I could not get this to succeed except on ASCII-only strings
