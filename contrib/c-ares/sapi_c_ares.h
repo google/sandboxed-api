d705a141a403eb66d66dd36f1f0b7d6789281f8c
Demi Marie Obenour <demi@invisiblethingslab.com>
2022-02-24 13:52:29 -0500

    Add C-Ares files

---
+++ contrib/c-ares/sapi_c_ares.h
@@ -0,0 +1,47 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef CONTRIB_CARES_CARES_SAPI_H_
+#define CONTRIB_CARES_CARES_SAPI_H_
+
+#include <syscall.h>
+
+#include "sandboxed_api/util/fileops.h"
+#include "cares_sapi.sapi.h"  // NOLINT(build/include)
+namespace c_ares_sapi {
+class AresSapiSandbox : public c_ares_sapi::AresSandbox {
+ public:
+  std::unique_ptr<sandbox2::Policy> ModifyPolicy(
+      sandbox2::PolicyBuilder*) override {
+    return sandbox2::PolicyBuilder()
+        .AllowSystemMalloc()
+        .AllowRead()
+        .AllowStat()
+        .AllowWrite()
+        .AllowExit()
+        .AllowSyscalls({
+            __NR_futex,
+            __NR_close,
+            __NR_lseek,
+            __NR_getpid,
+            __NR_clock_gettime,
+        })
+        .AllowLlvmSanitizers()
+        .BuildOrDie();
+  }
+};
+}
+
+#endif  // CONTRIB_CARES_CARES_SAPI_H_
+
