1755ba08e1a14fd5b1c05bba670c10691c5516fc
Wiktor Garbacz <wiktorg@google.com>
2023-03-27 08:13:20 -0700

    Internal Code Change
    PiperOrigin-RevId: 519725866
    Change-Id: Ibac005b875127ae68e28346fb78e74e789cff01e

--- sandboxed_api/sandbox2/examples/custom_fork/custom_fork_sandbox.cc
+++ sandboxed_api/sandbox2/examples/custom_fork/custom_fork_sandbox.cc
@@ -32,7 +32,6 @@
 #include "sandboxed_api/config.h"
 #include "sandboxed_api/sandbox2/comms.h"
 #include "sandboxed_api/sandbox2/executor.h"
-#include "sandboxed_api/sandbox2/forkserver.h"
 #include "sandboxed_api/sandbox2/limits.h"
 #include "sandboxed_api/sandbox2/policy.h"
 #include "sandboxed_api/sandbox2/policybuilder.h"
