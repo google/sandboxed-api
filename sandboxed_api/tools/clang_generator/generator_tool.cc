2a8d975003678c89d3554167453462b91df4c668
Oliver Kunz <okunz@google.com>
2024-07-02 03:19:02 -0700

    clang_generator: Refactor command-line argument handling.
    Remove unnecessary `static` and make use of `absl::NoDestructor` on
    the global variables. Furthermore, remove unnecessary  `sapi::` namespace identifiers.
    
    PiperOrigin-RevId: 648655427
    Change-Id: I8ab42c5391ca484bbb39af098cc7463f4528b4a5

--- sandboxed_api/tools/clang_generator/generator_tool.cc
+++ sandboxed_api/tools/clang_generator/generator_tool.cc
@@ -18,6 +18,7 @@
 #include <vector>
 
 #include "absl/base/attributes.h"
+#include "absl/base/no_destructor.h"
 #include "absl/status/status.h"
 #include "absl/strings/match.h"
 #include "absl/strings/str_cat.h"  // sapi::google3-only(internal feature only)
@@ -40,62 +41,66 @@
 namespace sapi {
 namespace {
 
-static auto* g_tool_category =
-    new llvm::cl::OptionCategory("Sandboxed API Options");
+absl::NoDestructor<llvm::cl::OptionCategory> g_tool_category(
+    "Sandboxed API Options");
 
-static auto* g_common_help =
-    new llvm::cl::extrahelp(clang::tooling::CommonOptionsParser::HelpMessage);
-static auto* g_extra_help = new llvm::cl::extrahelp(
+absl::NoDestructor<llvm::cl::extrahelp> g_common_help(
+    clang::tooling::CommonOptionsParser::HelpMessage);
+absl::NoDestructor<llvm::cl::extrahelp> g_extra_help(
     "Full documentation at: "
     "<https://developers.google.com/code-sandboxing/sandboxed-api>\n"
     "Report bugs to <https://github.com/google/sandboxed-api/issues>\n");
 
 // Command line options
-static auto* g_sapi_embed_dir = new llvm::cl::opt<std::string>(
+absl::NoDestructor<llvm::cl::opt<std::string>> g_sapi_embed_dir(
     "sapi_embed_dir", llvm::cl::desc("Directory with embedded includes"),
     llvm::cl::cat(*g_tool_category));
-static auto* g_sapi_embed_name = new llvm::cl::opt<std::string>(
+absl::NoDestructor<llvm::cl::opt<std::string>> g_sapi_embed_name(
     "sapi_embed_name", llvm::cl::desc("Identifier of the embed object"),
     llvm::cl::cat(*g_tool_category));
-static auto* g_sapi_functions = new llvm::cl::list<std::string>(
+absl::NoDestructor<llvm::cl::list<std::string>> g_sapi_functions(
+
     "sapi_functions", llvm::cl::CommaSeparated,
     llvm::cl::desc("List of functions to generate a Sandboxed API for. If "
                    "empty, generates a SAPI for all functions found."),
     llvm::cl::cat(*g_tool_category));
 ABSL_DEPRECATED("Pass the input files directly to the tool.")
-static auto* g_sapi_in = new llvm::cl::list<std::string>(
+absl::NoDestructor<llvm::cl::list<std::string>> g_sapi_in(
+
     "sapi_in", llvm::cl::CommaSeparated,
     llvm::cl::desc("List of input files to analyze (DEPRECATED)"),
     llvm::cl::cat(*g_tool_category));
 ABSL_DEPRECATED("Ignored for compatibility.")
-static auto* g_sapi_isystem = new llvm::cl::opt<std::string>(
+absl::NoDestructor<llvm::cl::opt<std::string>> g_sapi_isystem(
+
     "sapi_isystem",
     llvm::cl::desc(
         "Parameter file with extra system include paths (DEPRECATED)"),
     llvm::cl::cat(*g_tool_category));
-static auto* g_sapi_limit_scan_depth = new llvm::cl::opt<bool>(
+absl::NoDestructor<llvm::cl::opt<bool>> g_sapi_limit_scan_depth(
     "sapi_limit_scan_depth",
-    llvm::cl::desc(
-        "Whether to only scan for functions in the top-most translation unit"),
+    llvm::cl::desc("Whether to only scan for functions "
+                   "in the top-most translation unit"),
     llvm::cl::cat(*g_tool_category));
-static auto* g_sapi_name = new llvm::cl::opt<std::string>(
+absl::NoDestructor<llvm::cl::opt<std::string>> g_sapi_name(
+
     "sapi_name", llvm::cl::desc("Name of the Sandboxed API library"),
     llvm::cl::cat(*g_tool_category));
-static auto* g_sapi_ns = new llvm::cl::opt<std::string>(
+absl::NoDestructor<llvm::cl::opt<std::string>> g_sapi_ns(
     "sapi_ns", llvm::cl::desc("C++ namespace to wrap Sandboxed API class in"),
     llvm::cl::cat(*g_tool_category));
-static auto* g_sapi_out = new llvm::cl::opt<std::string>(
+absl::NoDestructor<llvm::cl::opt<std::string>> g_sapi_out(
     "sapi_out",
-    llvm::cl::desc(
-        "Output path of the generated header. If empty, simply appends .sapi.h "
-        "to the basename of the first source file specified."),
+    llvm::cl::desc("Output path of the generated header. If empty, simply "
+                   "appends .sapi.h "
+                   "to the basename of the first source file specified."),
     llvm::cl::cat(*g_tool_category));
 // sapi::google3-begin(internal feature only)
-static auto* g_safe_wrapper_gen = new llvm::cl::opt<bool>(
+absl::NoDestructor<llvm::cl::opt<bool>> g_safe_wrapper_gen(
     "safe_wrapper_gen",
     llvm::cl::desc("Whether to generate a safe drop-in replacement library."),
     llvm::cl::cat(*g_tool_category));
-static auto* g_force_safe_wrapper = new llvm::cl::opt<bool>(
+absl::NoDestructor<llvm::cl::opt<bool>> g_force_safe_wrapper(
     "force_safe_wrapper",
     llvm::cl::desc("Whether to overwrite an existing safe drop-in "
                    "replacement library in the active workspace."),
@@ -107,14 +112,13 @@ static auto* g_force_safe_wrapper = new llvm::cl::opt<bool>(
 GeneratorOptions GeneratorOptionsFromFlags(
     const std::vector<std::string>& sources) {
   GeneratorOptions options;
-  options.work_dir = sapi::file_util::fileops::GetCWD();
+  options.work_dir = file_util::fileops::GetCWD();
   options.set_function_names(*g_sapi_functions);
   for (const auto& input : sources) {
     // Keep absolute paths as is, turn
-    options.in_files.insert(
-        absl::StartsWith(input, "/")
-            ? input
-            : sapi::file::JoinPath(options.work_dir, input));
+    options.in_files.insert(absl::StartsWith(input, "/")
+                                ? input
+                                : file::JoinPath(options.work_dir, input));
   }
   options.set_limit_scan_depth(*g_sapi_limit_scan_depth);
   options.name = *g_sapi_name;
@@ -132,7 +136,7 @@ GeneratorOptions GeneratorOptionsFromFlags(
 
 absl::Status GeneratorMain(int argc, char* argv[]) {
   auto expected_opt_parser = OptionsParser::create(
-      argc, const_cast<const char**>(argv), *sapi::g_tool_category,
+      argc, const_cast<const char**>(argv), *g_tool_category,
       llvm::cl::ZeroOrMore,
       "Generates a Sandboxed API header for C/C++ translation units.");
   if (!expected_opt_parser) {
@@ -141,14 +145,14 @@ absl::Status GeneratorMain(int argc, char* argv[]) {
   OptionsParser& opt_parser = expected_opt_parser.get();
 
   std::vector<std::string> sources = opt_parser.getSourcePathList();
-  for (const auto& sapi_in : *sapi::g_sapi_in) {
+  for (const auto& sapi_in : *g_sapi_in) {
     sources.push_back(sapi_in);
   }
   if (sources.empty()) {
     return absl::InvalidArgumentError("Error: No input files.");
   }
 
-  auto options = sapi::GeneratorOptionsFromFlags(sources);
+  auto options = GeneratorOptionsFromFlags(sources);
 
   std::unique_ptr<clang::tooling::CompilationDatabase> db =
       FromCxxAjustedCompileCommands(
@@ -164,7 +168,7 @@ absl::Status GeneratorMain(int argc, char* argv[]) {
   // sapi::google3-begin(internal feature only)
   // Process safe drop-in generation.
   if (options.safe_wrapper_gen) {
-    sapi::SafeReplacementEmitter safe_emitter;
+    SafeReplacementEmitter safe_emitter;
     std::string base_file_path = file::JoinPath(
         options.embed_dir,
         StringReplace(options.embed_name, "sandboxed_", "safe_", false));
@@ -172,10 +176,10 @@ absl::Status GeneratorMain(int argc, char* argv[]) {
     std::string safe_wrapper_implementation_path =
         absl::StrCat(base_file_path, ".cc");
 
-    if (sapi::file_util::fileops::Exists(safe_wrapper_header_path,
-                                         /*fully_resolve=*/true) ||
-        sapi::file_util::fileops::Exists(safe_wrapper_implementation_path,
-                                         /*fully_resolve=*/true)) {
+    if (file_util::fileops::Exists(safe_wrapper_header_path,
+                                   /*fully_resolve=*/true) ||
+        file_util::fileops::Exists(safe_wrapper_implementation_path,
+                                   /*fully_resolve=*/true)) {
       if (!options.force_safe_wrapper) {
         return absl::UnknownError(
             "Error: Safe drop-in replacement library already exists. To "
@@ -184,38 +188,37 @@ absl::Status GeneratorMain(int argc, char* argv[]) {
     }
 
     if (int result = tool.run(
-            std::make_unique<sapi::GeneratorFactory>(safe_emitter, options)
-                .get());
+            std::make_unique<GeneratorFactory>(safe_emitter, options).get());
         result != 0) {
       return absl::UnknownError("Error: Header generation failed.");
     }
 
     SAPI_ASSIGN_OR_RETURN(std::string safe_wrapper_header,
                           safe_emitter.EmitSafeDropInHeader(options));
-    SAPI_RETURN_IF_ERROR(sapi::file::SetContents(
-        safe_wrapper_header_path, safe_wrapper_header, sapi::file::Defaults()));
+    SAPI_RETURN_IF_ERROR(file::SetContents(
+        safe_wrapper_header_path, safe_wrapper_header, file::Defaults()));
 
     SAPI_ASSIGN_OR_RETURN(std::string safe_wrapper_implementation,
                           safe_emitter.EmitSafeDropInImplementation(options));
-    SAPI_RETURN_IF_ERROR(sapi::file::SetContents(
-        safe_wrapper_implementation_path, safe_wrapper_implementation,
-        sapi::file::Defaults()));
+    SAPI_RETURN_IF_ERROR(file::SetContents(safe_wrapper_implementation_path,
+                                           safe_wrapper_implementation,
+                                           file::Defaults()));
 
     return absl::OkStatus();
   }
   // sapi::google3-end
 
   // Process SAPI header generation.
-  sapi::Emitter emitter;
-  if (int result = tool.run(
-          std::make_unique<sapi::GeneratorFactory>(emitter, options).get());
+  Emitter emitter;
+  if (int result =
+          tool.run(std::make_unique<GeneratorFactory>(emitter, options).get());
       result != 0) {
     return absl::UnknownError("Error: Header generation failed.");
   }
 
   SAPI_ASSIGN_OR_RETURN(std::string header, emitter.EmitHeader(options));
-  SAPI_RETURN_IF_ERROR(sapi::file::SetContents(options.out_file, header,
-                                               sapi::file::Defaults()));
+  SAPI_RETURN_IF_ERROR(
+      file::SetContents(options.out_file, header, file::Defaults()));
   return absl::OkStatus();
 }
 
