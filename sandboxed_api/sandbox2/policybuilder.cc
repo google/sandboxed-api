efac7f9049c1299656b2c7b4b39b3a66785b4569
Oliver Kunz <okunz@google.com>
2025-09-19 07:11:30 -0700

    policybuilder: Remove deprecated `PolicyBuilder::AllowInotifyInit()`.
    The `AllowInotifyInit()` method has been deprecated in favor of `AllowInotify(). All usages have been migrated, so this CL removes the deprecated function.
    
    PiperOrigin-RevId: 809025736
    Change-Id: I1412006b0f16a95533355b1cd87297fc67b20e54

--- sandboxed_api/sandbox2/policybuilder.cc
+++ sandboxed_api/sandbox2/policybuilder.cc
@@ -292,17 +292,6 @@ PolicyBuilder& PolicyBuilder::AllowEpoll() {
   return AllowEpollWait();
 }
 
-PolicyBuilder& PolicyBuilder::AllowInotifyInit() {
-  return AllowSyscalls({
-#ifdef __NR_inotify_init
-      __NR_inotify_init,
-#endif
-#ifdef __NR_inotify_init1
-      __NR_inotify_init1,
-#endif
-  });
-}
-
 PolicyBuilder& PolicyBuilder::AllowInotify() {
   return AllowSyscalls({
 #ifdef __NR_inotify_init
