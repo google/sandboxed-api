4dffbb3ab63b8f15fa330bb7e21d89b9358ca228
Wiktor Garbacz <wiktorg@google.com>
2026-03-19 01:07:14 -0700

    Internal change
    PiperOrigin-RevId: 886022724
    Change-Id: Icab421391c30814b3516754fac4dae2f999ae505

--- sandboxed_api/sandbox2/policybuilder.cc
+++ sandboxed_api/sandbox2/policybuilder.cc
@@ -52,6 +52,7 @@
 #include "absl/status/statusor.h"
 #include "absl/strings/match.h"
 #include "absl/strings/str_cat.h"
+#include "absl/strings/str_split.h"
 #include "absl/strings/string_view.h"
 #include "absl/types/span.h"
 #include "sandboxed_api/config.h"
