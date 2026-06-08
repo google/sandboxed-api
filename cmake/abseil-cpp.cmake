20ffdc55ce2ff8f351143aa30846fbd53175cdb6
Christian Blichmann <cblichmann@google.com>
2026-06-08 06:07:36 -0700

    Update Abseil-cpp and rules_cc versions
    - Update the `abseil-cpp` dependency to version `20260526.0` and `rules_cc` to `0.2.18` in MODULE.bazel
    - Update the corresponding Abseil-cpp download URL and hash in cmake/abseil-cpp.cmake
    
    PiperOrigin-RevId: 928519616
    Change-Id: Ib420d4a1498b310f73b55cb8198705fa6c2ab981

--- cmake/abseil-cpp.cmake
+++ cmake/abseil-cpp.cmake
@@ -13,8 +13,8 @@
 # limitations under the License.
 
 FetchContent_Declare(absl
-  URL https://github.com/abseil/abseil-cpp/releases/download/20260107.1/abseil-cpp-20260107.1.tar.gz
-  URL_HASH SHA256=4314e2a7cbac89cac25a2f2322870f343d81579756ceff7f431803c2c9090195
+  URL https://github.com/abseil/abseil-cpp/releases/download/20260526.0/abseil-cpp-20260526.0.tar.gz
+  URL_HASH SHA256=6e1aee535473414164bf83e4ebc40240dec71a4701f8a642d906e95bea1aea0c
 )
 set(ABSL_CXX_STANDARD ${SAPI_CXX_STANDARD} CACHE STRING "" FORCE)
 set(ABSL_PROPAGATE_CXX_STD ON CACHE BOOL "" FORCE)
